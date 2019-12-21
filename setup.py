abd21bc45938d1eab3006f847359ab599ffb5d30
Matthias Schmieder <matthias.schmieder@stryker.com>
2019-12-21 16:55:00 +0100

    reading version

--- setup.py
+++ setup.py
@@ -1,27 +1,32 @@
 from setuptools import setup
 from os import path
+import re
 
 # read the contents of your README file
 this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+version = re.search(r'^__version__\s*=\s*"(.*)"',
+                    open('kacl/__init__.py').read(),
+                    re.M).group(1)
+
 setup(name='python-kacl',
-      version='0.1.0',
+      version=version,
       description='Pyhton module and CLI tool for validating and modifying Changelogs in "keep-a-changelog" format"',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='http://github.com/mschmieder/python-kacl',
       author='Matthias Schmieder',
       author_email='schmieder.matthias@gmail.com',
-       entry_points = {
-        "console_scripts": ['kacl-cli = kacl.kacl_cli:start']
+      entry_points={
+           "console_scripts": ['kacl-cli = kacl.kacl_cli:start']
       },
       license='MIT',
       packages=['kacl'],
       install_requires=[
-        'click',
-        'semver',
-        'pychalk'
+          'click',
+          'semver',
+          'pychalk'
       ],
       zip_safe=False)
