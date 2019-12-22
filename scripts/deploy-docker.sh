b43d5a20b4f33ed384ecc2042a0780825acc2870
Matthias Schmieder <matthias.schmieder@stryker.com>
2019-12-22 14:40:12 +0100

    added docker deployment

---
+++ scripts/deploy-docker.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+version=$(cat VERSION)
+
+docker build mschmieder/kacl-cli:${version}
+docker push mschmieder/kacl-cli:${version}
+docker tag mschmieder/kacl-cli:${version} mschmieder/kacl-cli:latest
+docker push mschmieder/kacl-cli:latest
