414143c82d20241a7cac56b211017074fc27d601
Matthias Schmieder <matthias.schmieder@stryker.com>
2019-12-22 17:02:25 +0100

    testing with docker hub description

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -7,3 +7,11 @@ docker build -t mschmieder/kacl-cli:${version} .
 docker push mschmieder/kacl-cli:${version}
 docker tag mschmieder/kacl-cli:${version} mschmieder/kacl-cli:latest
 docker push mschmieder/kacl-cli:latest
+
+# add changelog to docker hub
+docker run -v $PWD:/workspace \
+    -e DOCKERHUB_USERNAME=$DOCKER_USERNAME \
+    -e DOCKERHUB_PASSWORD=$DOCKER_PASSWORD \
+    -e DOCKERHUB_REPOSITORY="mschmieder/kacl-cli" \
+    -e README_FILEPATH='/workspace/README.md' \
+    peterevans/dockerhub-description:2.1.0
