2a9018f5acc6576bc3567d4462227510b7b27a7e
Matthias Schmieder <matthias.schmieder@stryker.com>
2019-12-22 16:03:02 +0100

    fixed missing -t flag

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -3,7 +3,7 @@ echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 version=$(cat VERSION)
 
-docker build mschmieder/kacl-cli:${version} .
+docker build -t mschmieder/kacl-cli:${version} .
 docker push mschmieder/kacl-cli:${version}
 docker tag mschmieder/kacl-cli:${version} mschmieder/kacl-cli:latest
 docker push mschmieder/kacl-cli:latest
