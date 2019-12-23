0fd72b7c0feb68427f9fb64e1074e5d62cfd877a
Matthias Schmieder <matthias.schmieder@stryker.com>
2019-12-23 14:18:48 +0100

    fixed release script

--- scripts/release-version.sh
+++ scripts/release-version.sh
@@ -12,6 +12,7 @@ bumpversion --allow-dirty ${VERSION_INCREMENT_SECTION}
 new_version=$(cat VERSION)
 
 python3 kacl/kacl_cli.py release $(cat VERSION) --modify
-git add CHANGELOG.md kacl/__init__.py VERSION
-
-echo git commit -m "Version update ${current_version} --> ${next_version}"
+git add CHANGELOG.md kacl/__init__.py VERSION .bumpversion.cfg
+git commit -m "Version update ${current_version} --> ${new_version}"
+git tag v${new_version}
+git push master --tags
