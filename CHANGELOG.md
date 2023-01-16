cd9e77806dd26d42ffd50828a166297f437af1d0
Matthias Schmieder <matthias.schmieder@stryker.com>
2023-01-16 12:29:24 +0100

    removed python-box dependency

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ## [Unreleased]
+### Changed
+- removed `python-box` dependency
 
 ## [0.3.1] - 2023-01-15
 ### Fixed
--- kacl/config.py
+++ kacl/config.py
@@ -1,6 +1,5 @@
 import os
 import yaml
-from box import Box
 
 class KACLConfig:
     def __init__(self, config_file=None):
@@ -11,60 +10,58 @@ class KACLConfig:
             with open(os.path.expanduser(config_file), 'r') as f:
                 KACLConfig.merge(self.__config, yaml.safe_load(f))
 
-        self.__config = Box(self.__config)
-
     def allowed_header_titles(self):
-        return self.__config.kacl.allowed_header_titles
+        return self.__config['kacl']['allowed_header_titles']
 
     def allowed_version_sections(self):
-        return self.__config.kacl.allowed_version_sections
+        return self.__config['kacl']['allowed_version_sections']
 
     def default_content(self):
-        return self.__config.kacl.default_content
+        return self.__config['kacl']['default_content']
 
     def git_tag_name(self):
-        return self.__config.kacl.git.tag_name
+        return self.__config['kacl']['git']['tag_name']
 
     def git_tag_description(self):
-        return self.__config.kacl.git.tag_description
+        return self.__config['kacl']['git']['tag_description']
 
     def changelog_file_path(self):
-        return self.__config.kacl.file
+        return self.__config['kacl']['file']
 
     def set_changelog_file_path(self, file_path):
-        self.__config.kacl.file = file_path
+        self.__config['kacl']['file'] = file_path
 
     def git_create_tag(self):
-        return self.__config.kacl.git.tag
+        return self.__config['kacl']['git']['tag']
 
     def git_create_commit(self):
-        return self.__config.kacl.git.commit
+        return self.__config['kacl']['git']['commit']
 
     def git_commit_message(self):
-        return self.__config.kacl.git.commit_message
+        return self.__config['kacl']['git']['commit_message']
 
     def git_commit_additional_files(self):
-        return self.__config.kacl.git.commit_additional_files
+        return self.__config['kacl']['git']['commit_additional_files']
 
     def link_auto_generate(self):
-        if 'auto_generate' in self.__config.kacl.links:
-            return self.__config.kacl.links.auto_generate
+        if 'auto_generate' in self.__config['kacl']['links']:
+            return self.__config['kacl']['links']['auto_generate']
 
     def link_host_url(self):
-        if 'host_url' in self.__config.kacl.links:
-            return self.__config.kacl.links.host_url
+        if 'host_url' in self.__config['kacl']['links']:
+            return self.__config['kacl']['links']['host_url']
 
     def links_compare_versions_template(self):
-        if 'compare_versions_template' in self.__config.kacl.links:
-            return self.__config.kacl.links.compare_versions_template
+        if 'compare_versions_template' in self.__config['kacl']['links']:
+            return self.__config['kacl']['links']['compare_versions_template']
 
     def links_unreleased_changes_template(self):
-        if 'unreleased_changes_template' in self.__config.kacl.links:
-            return self.__config.kacl.links.unreleased_changes_template
+        if 'unreleased_changes_template' in self.__config['kacl']['links']:
+            return self.__config['kacl']['links']['unreleased_changes_template']
 
     def links_initial_version_template(self):
-        if 'initial_version_template' in self.__config.kacl.links:
-            return self.__config.kacl.links.initial_version_template
+        if 'initial_version_template' in self.__config['kacl']['links']:
+            return self.__config['kacl']['links']['initial_version_template']
 
 
     @staticmethod
