816e400a14559ace03fbde61e877c0e3aaf44c98
Matthias Schmieder <matthias.schmieder@stryker.com>
2023-01-24 12:43:21 +0100

    disabled dockerhub description

--- scripts/deploy-docker.sh
+++ scripts/deploy-docker.sh
@@ -8,10 +8,10 @@ docker push mschmieder/kacl-cli:${version}
 docker tag mschmieder/kacl-cli:${version} mschmieder/kacl-cli:latest
 docker push mschmieder/kacl-cli:latest
 
-# add changelog to docker hub
-docker run -v $PWD:/workspace \
-    -e DOCKERHUB_USERNAME=$DOCKER_USERNAME \
-    -e DOCKERHUB_PASSWORD=$DOCKER_PASSWORD \
-    -e DOCKERHUB_REPOSITORY="mschmieder/kacl-cli" \
-    -e README_FILEPATH='/workspace/README.md' \
-    peterevans/dockerhub-description:2.1.0
+# # add changelog to docker hub
+# docker run -v $PWD:/workspace \
+#     -e DOCKERHUB_USERNAME=$DOCKER_USERNAME \
+#     -e DOCKERHUB_PASSWORD=$DOCKER_PASSWORD \
+#     -e DOCKERHUB_REPOSITORY="mschmieder/kacl-cli" \
+#     -e README_FILEPATH='/workspace/README.md' \
+#     peterevans/dockerhub-description:2.1.0
